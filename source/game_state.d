919d8a5f0653532282df9762d60bc0f942ee12a1
Zane Sterling <sterling.zane@gmail.com>
2016-07-20 22:06:20 -0700

    Implement io

--- source/game_state.d
+++ source/game_state.d
@@ -1,10 +1,31 @@
 module game_state;
 
+import derelict.sdl2.sdl;
+
+import level;
+
 struct GameState {
 	bool running = true;
 	Level curLevel;
 
 	void update() {
+		updateIO();
 		curLevel.update();
+	}
+
+	void updateIO() {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_KEYDOWN:
+					pressKey(event.key.keysym.sym);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	void pressKey(SDL_Keycode keycode) {
 	}
 }
